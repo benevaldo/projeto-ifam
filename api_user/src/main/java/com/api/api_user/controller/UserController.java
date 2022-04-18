dcc2fa4e2f3244c97a87dcc42378f799562235de
bernardo <bernadigo@gmail.com>
2022-04-18 16:17:08 -0400

    tratamento da operação put para nao adiconar quando nao existe

--- api_user/src/main/java/com/api/api_user/controller/UserController.java
+++ api_user/src/main/java/com/api/api_user/controller/UserController.java
@@ -51,8 +51,7 @@ public class UserController {
   }
 
   @DeleteMapping(value = "/{id}")
-  public ResponseDto deleteUser(@PathVariable Long id) {
-
+  public ResponseDto deleteUser(@Valid @PathVariable Long id) {
     return userService.deleteUser(id);
   }
 
